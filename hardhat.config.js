1aef77be1c75cf8d9f4c8f772d31fd6acc87c66d
= <patrick@WIN.localdomain>
2022-09-24 17:49:48 +0800

    测试网络以及etherscan合约认证配置

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+const secrets = require("./secret.json");
 
+require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.9",
+  etherscan: {
+    apiKey: secrets.ETH_API_KEY
+  },
   networks : {
     hardhat:{
       forking :{
         url: "https://eth-mainnet.g.alchemy.com/v2/613t3mfjTevdrCwDl28CVvuk6wSIxRPi"
 
-      }
-         }
+            }
+         },
+      goerli : {
+        url: `https://eth-goerli.alchemyapi.io/v2/${secrets.ALCHEMY_API_KEY}`,
+        accounts: [secrets.PRIVATE_KEY]
+
+  }
+  
   }
 };
